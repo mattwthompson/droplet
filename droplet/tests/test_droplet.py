a715176760b86ea05bf38bb355937ddb46f8aa09
matt.thompson@vanderbilt.edu <matt.thompson@vanderbilt.edu>
2017-12-14 08:49:48 -0600

    Update test

--- droplet/tests/test_droplet.py
+++ droplet/tests/test_droplet.py
@@ -7,7 +7,7 @@ c_atoms = trj.topology.select('not resname RES')
 trj.atom_slice(c_atoms, inplace=True)
 
 drop = calc_contact_angle(trj, z_surf=2.682, z_max=4, r_range=(0, 6),
-                          n_bins=50, trim_z=0.8, rho_cutoff=20,
+                          n_bins=50, trim_z=0.8, trim_r=0.4, rho_cutoff=20,
                           direction='top')
 
 assert 50 < drop['theta'] < 52
